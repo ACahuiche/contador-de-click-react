892d93fc0121689caa97273180b01ab4b61db176
ACahuiche <alberto.cahuiche@outlook.com>
2022-09-26 22:13:51 -0500

    Se agrega App.css y se agrega logo a la pantalla principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freecodecamplogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+        className="freecodecamp-logo"
+        src={freecodecamplogo}
+        alt="Logo de freecodecamp" />
+      </div>
     </div>
   );
 }
